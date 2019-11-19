c56b9170dea601f4789bf97eb7723cc137c0a182
Asem Soufi <asemsoufi@gmail.com>
2019-11-19 10:44:10 +0000

    importing Battleships code to add JavaFX GUI part

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        //Game thisGame = new Game();
 
         System.out.println("Game is on..");
         Game game = new Game();
